998f40918df0af8c28c2f116bb7dd2c3c06f498e
Charlie <84060702+cjayversace@users.noreply.github.com>
2022-09-27 15:17:20 -0400

    Add files via upload

---
+++ app/src/main/java/com/example/flixter/Movie.kt
@@ -0,0 +1,40 @@
+package com.example.flixter
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+import org.json.JSONArray
+
+@Parcelize
+data class Movie (
+    val movieId: Int,
+    val voteAvg: Double,
+    val title: String,
+    val overview: String,
+    private val posterPath: String
+) : Parcelable {
+
+    val posterImageUrl ="https://image.tmdb.org/t/p/w342/$posterPath"
+
+
+    companion object{
+        fun fromJSONarr(movieArr: JSONArray) :List<Movie> {
+            val movies = mutableListOf<Movie>()
+            for(i in 0 until movieArr.length()){
+                val movieJson = movieArr.getJSONObject(i)
+                movies.add(
+                    Movie(
+                        movieJson.getInt("id"),
+                        movieJson.getDouble("vote_average"),
+                        movieJson.getString("title"),
+                        movieJson.getString("overview"),
+                        movieJson.getString("poster_path")
+
+                    )
+                )
+            }
+            return movies
+        }
+
+    }
+
+}
